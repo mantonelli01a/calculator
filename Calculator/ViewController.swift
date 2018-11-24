501ba1f2d5b1d2d469246f3ec3b3f3a810557f3b
mantonelli01a <mike@mantonelli.plus.com>
2018-11-24 09:16:14 +0000

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Administrator on 24/11/2018.
+//  Copyright © 2018 DoubleThunder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
